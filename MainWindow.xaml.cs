c8eb6038ec54011ae8537fcf40081de6f8b0ef72
Mario Guggenberger <mg@protyposis.net>
2010-09-12 17:37:32 +0000

    very simple control to display waveform + demo application

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NAudio.Wave;
+
+namespace AudioAlign {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e) {
+            // http://msdn.microsoft.com/en-us/library/aa969773.aspx
+            // Configure open file dialog box
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "audio"; // Default file name
+            dlg.DefaultExt = ".wav"; // Default file extension
+            dlg.Filter = "Wave files (.wav)|*.wav"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true) {
+                // Open document
+                string filename = dlg.FileName;
+                WaveStream waveStream = new WaveFileReader(filename);
+                waveView.WaveStream = waveStream;
+            }
+        }
+    }
+}
