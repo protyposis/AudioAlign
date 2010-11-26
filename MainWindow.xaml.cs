c732ec3eda1efe0024affc70ce1df478025627b6
Mario Guggenberger <mg@protyposis.net>
2010-11-26 15:56:48 +0000

    * FEATURE WaveView testapp (code migrated from AudioAlign app / AudioAlign app reverted to empty app)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,34 +11,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using AudioAlign.Audio;
 
-namespace AudioAlign {
+namespace AudioAlign.Test.WaveViewControl {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, RoutedEventArgs e) {
-            // http://msdn.microsoft.com/en-us/library/aa969773.aspx
-            // Configure open file dialog box
-            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
-            dlg.FileName = "audio"; // Default file name
-            dlg.DefaultExt = ".wav"; // Default file extension
-            dlg.Filter = "Wave files (.wav)|*.wav"; // Filter files by extension
-
-            // Show open file dialog box
-            Nullable<bool> result = dlg.ShowDialog();
-
-            // Process open file dialog box results
-            if (result == true) {
-                // Open document
-                string filename = dlg.FileName;
-                waveView.AudioStream = AudioStreamFactory.FromFilenameForGUI(filename);
-            }
         }
     }
 }
