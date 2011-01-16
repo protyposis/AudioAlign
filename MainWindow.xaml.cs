fa0dee3bd9535cda3fb7c7ca82a23bc61fb31da0
Mario Guggenberger <mg@protyposis.net>
2011-01-16 15:08:42 +0000

    * FEATURE PhaseInversionStream inverts the phase of a track * FEATURE 2 new icons * FEATURE MultitrackPlayer encapsulates and handles all NAudio playback stuff for multiple AudioTracks * IMPROVEMENT AudioAlign integration of test app features * IMPROVEMENT AudioAlign main window icon * FEATURE audio track phase inversion * BUGFIX TrackList EventArgs duplicated generic type removed * REFACTORE INotifyPropertyChanged moved from AudioTrack to base Track * IMPROVEMENT change notification for Track properties * IMPROVEMENT LogSlider default style created (necessary for themes to be automatically applied) * BUGFIX AudioAlign MainWindow namespace corrected

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,14 +16,21 @@ using System.IO;
 using AudioAlign.WaveControls;
 using AudioAlign.Audio;
 using System.Diagnostics;
+using System.Windows.Threading;
 
-namespace AudioAlign.Test.MultitrackControls {
+namespace AudioAlign {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        private TrackList<AudioTrack> trackList;
+        private MultitrackPlayer player;
+
         public MainWindow() {
             InitializeComponent();
+
+            trackList = new TrackList<AudioTrack>();
         }
 
         private void btnAddWaveform_Click(object sender, RoutedEventArgs e) {
@@ -40,12 +47,90 @@ namespace AudioAlign.Test.MultitrackControls {
             // Process open file dialog box results
             if (result == true) {
                 // Open document
-                AudioTrack audioTrack = new AudioTrack(new FileInfo(dlg.FileName));
-                //WaveView waveView = new WaveView();
-                //waveView.AudioStream = AudioStreamFactory.FromAudioTrackForGUI(audioTrack);
-                //multiTrackPanel.Children.Add(waveView);
-                multiTrackViewer1.Items.Add(audioTrack);
+                AddFile(dlg.FileName);
             }
+        }
+
+        private void AddFile(string fileName) {
+            if (AudioStreamFactory.IsSupportedFile(fileName)) {
+                AudioTrack audioTrack = new AudioTrack(new FileInfo(fileName));
+                multiTrackViewer1.Items.Add(audioTrack);
+                trackList.Add(audioTrack);
+            }
+        }
+
+        private void multiTrackViewer1_Drop(object sender, DragEventArgs e) {
+            // source: http://stackoverflow.com/questions/332859/detect-dragndrop-file-in-wpf
+            if (e.Data is DataObject && ((DataObject)e.Data).ContainsFileDropList()) {
+                foreach (string filePath in ((DataObject)e.Data).GetFileDropList()) {
+                    AddFile(filePath);
+                }
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            CommandBinding playBinding = new CommandBinding(MediaCommands.Play);
+            CommandBindings.Add(playBinding);
+
+            CommandBinding pauseBinding = new CommandBinding(MediaCommands.Pause);
+            CommandBindings.Add(pauseBinding);
+
+            player = new MultitrackPlayer(trackList);
+
+            playBinding.CanExecute += new CanExecuteRoutedEventHandler(playCommandBinding_CanExecute);
+            playBinding.Executed += new ExecutedRoutedEventHandler(playCommandBinding_Executed);
+            pauseBinding.CanExecute += new CanExecuteRoutedEventHandler(pauseCommandBinding_CanExecute);
+            pauseBinding.Executed += new ExecutedRoutedEventHandler(pauseCommandBinding_Executed);
+
+            player.VolumeAnnounced += new EventHandler<Audio.NAudio.StreamVolumeEventArgs>(
+                delegate(object sender2, Audio.NAudio.StreamVolumeEventArgs e2) {
+                    if (e2.MaxSampleValues.Length >= 2) {
+                        stereoVUMeter1.AmplitudeLeft = e2.MaxSampleValues[0];
+                        stereoVUMeter1.AmplitudeRight = e2.MaxSampleValues[1];
+                    }
+                });
+
+            player.CurrentTimeChanged += new EventHandler<ValueEventArgs<TimeSpan>>(
+                delegate(object sender2, ValueEventArgs<TimeSpan> e2) {
+                    multiTrackViewer1.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+                        new DispatcherOperationCallback(delegate {
+                            multiTrackViewer1.VirtualCaretOffset = e2.Value.Ticks;
+                            return null;
+                    }), null);
+                });
+
+            player.PlaybackStateChanged += new EventHandler(
+                delegate(object sender2, EventArgs e2) {
+                    CommandManager.InvalidateRequerySuggested();
+                });
+
+            volumeSlider.ValueChanged += new RoutedPropertyChangedEventHandler<double>(
+                delegate(object sender2, RoutedPropertyChangedEventArgs<double> e2) {
+                    player.Volume = (float)e2.NewValue;
+                });
+        }
+
+        private void Window_Closed(object sender, EventArgs e) {
+            player.Dispose();
+        }
+
+        private void playCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = player.CanPlay;
+            e.Handled = true;
+        }
+
+        private void pauseCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e) {
+            e.CanExecute = player.CanPause;
+            e.Handled = true;
+        }
+
+        private void playCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e) {
+            player.CurrentTime = new TimeSpan(multiTrackViewer1.VirtualCaretOffset);
+            player.Play();
+        }
+
+        private void pauseCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e) {
+            player.Pause();
         }
     }
 }
