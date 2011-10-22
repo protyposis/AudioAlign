e662211246d21f69f6065774a91b6629aa2719d9
Mario Guggenberger <mg@protyposis.net>
2011-10-22 13:07:18 +0000

    * BUGFIX CrossCorrelation.Calculate progress reporting * BUGFIX correlation matching resulted in invalid match at track start

--- MatchingWindow.xaml.cs
+++ MatchingWindow.xaml.cs
@@ -555,7 +555,7 @@ namespace AudioAlign {
                             Interval t2Interval = new Interval((t2Offset + position).Ticks, (t2Offset + position + window).Ticks);
 
                             if (t1Interval.TimeTo >= trackPair.Track1.Length || t2Interval.TimeTo >= trackPair.Track2.Length) {
-                                // not enough samples remaining to compute the correlation
+                                // not enough samples remaining to compute the correlation (end of track reached)
                                 return;
                             }
 
@@ -563,9 +563,12 @@ namespace AudioAlign {
                                 trackPair.Track1.CreateAudioStream(), t1Interval,
                                 trackPair.Track2.CreateAudioStream(), t2Interval,
                                 progressMonitor);
+                            // always apply a positive offset that moves the match position inside the corelation interval,
+                            // else it can happen that a negative offset is applied to a match at the beginning of the stream
+                            // which means that the matching point would be at a negative position in the audio stream
                             computedMatches.Add(new Match {
-                                Track1 = trackPair.Track1, Track1Time = t1Offset + position,
-                                Track2 = trackPair.Track2, Track2Time = t2Offset + position + offset,
+                                Track1 = trackPair.Track1, Track1Time = t1Offset + position + (offset < TimeSpan.Zero ? -offset : TimeSpan.Zero),
+                                Track2 = trackPair.Track2, Track2Time = t2Offset + position + (offset >= TimeSpan.Zero ? offset : TimeSpan.Zero),
                                 Similarity = 1
                             });
                         }
