f990d1a2c72941e62de550cd701e7fc7aff9b26a
Mario Guggenberger <mg@protyposis.net>
2015-03-19 20:58:29 +0100

    Replace DynamicDataDisplay with OxyPlot in the CrossCorrelationResult window

--- CrossCorrelationResult.xaml.cs
+++ CrossCorrelationResult.xaml.cs
@@ -11,9 +11,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using AudioAlign.Audio.Matching;
-using Microsoft.Research.DynamicDataDisplay;
-using Microsoft.Research.DynamicDataDisplay.DataSources;
-using Microsoft.Research.DynamicDataDisplay.PointMarkers;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Annotations;
+using OxyPlot.Axes;
 
 namespace AudioAlign {
     /// <summary>
@@ -27,39 +28,76 @@ namespace AudioAlign {
             PlotResult(plotter1, ccr);
             PlotResult(plotter2, ccr.AbsoluteResult());
 
-            plotter1.Viewport.SetBinding(Viewport2D.VisibleProperty,
-                                         new Binding("Visible") {Source = plotter2.Viewport, Mode = BindingMode.TwoWay});
+            //plotter1.Viewport.SetBinding(Viewport2D.VisibleProperty,
+            //                             new Binding("Visible") {Source = plotter2.Viewport, Mode = BindingMode.TwoWay});
         }
 
-        private void PlotResult(ChartPlotter plotter, CrossCorrelation.Result ccr) {
+        private void PlotResult(OxyPlot.Wpf.Plot plotter, CrossCorrelation.Result ccr) {
             int[] xValues = new int[ccr.Correlations.Length];
-            for (int i = 0; i < xValues.Length; i++) {
+            List<DataPoint> values = new List<DataPoint>();
+            for (int i = 0; i < ccr.Correlations.Length; i++) {
                 xValues[i] = i;
+                values.Add(new DataPoint(i, ccr.Correlations[i]));
             }
 
-            var dsx = new EnumerableDataSource<int>(xValues);
-            var dsy = new EnumerableDataSource<float>(ccr.Correlations);
-            var ds = new CompositeDataSource(dsx, dsy);
-            dsx.SetXMapping(x => x);
-            dsy.SetYMapping(y => y);
-            plotter.AddLineGraph(ds, Colors.CornflowerBlue, 1d, "Correlation");
+            var plotModel = new PlotModel();
 
-            var dsmax = new RawDataSource(new Point(0, ccr.MaxValue), new Point(xValues.Length - 1, ccr.MaxValue));
-            plotter.AddLineGraph(dsmax, Colors.Green, 1d, "Max");
+            // setup plotter axes and viewport
+            plotModel.Axes.Add(new LinearAxis() {
+                Position = AxisPosition.Bottom,
+                Minimum = 0,
+                Maximum = xValues.Length
+            });
+            plotModel.Axes.Add(new LinearAxis() {
+                Minimum = -1,
+                Maximum = 2,
+                MajorGridlineStyle = LineStyle.Automatic,
+                MinorGridlineStyle = LineStyle.Automatic
+            });
 
-            var dsmaxmarker = new RawDataSource(new Point(ccr.MaxIndex, ccr.MaxValue));
-            plotter.AddLineGraph(dsmaxmarker, new Pen(Brushes.Magenta, 1d), new CirclePointMarker(), new PenDescription("Max"));
+            // correlation series
+            plotModel.Series.Add(new LineSeries {
+                ItemsSource = values,
+                Color = OxyColors.CornflowerBlue,
+                Title = "Correlation"
+            });
+
+            // distinct values
+            plotModel.Annotations.Add(new LineAnnotation {
+                Type = LineAnnotationType.Horizontal,
+                Y = ccr.MaxValue,
+                Color = OxyColors.Green,
+                ClipByXAxis = false
+            });
+
+            plotModel.Annotations.Add(new PointAnnotation {
+                X = ccr.MaxIndex,
+                Y = ccr.MaxValue,
+                Fill = OxyColors.Red,
+                Text = "Max"
+            });
+
 
             var avg = ccr.Correlations.Average();
-            var dsavg = new RawDataSource(new Point(0, avg), new Point(xValues.Length - 1, avg));
-            plotter.AddLineGraph(dsavg, Colors.Red, 1d, "Avg");
+            plotModel.Annotations.Add(new LineAnnotation {
+                Type = LineAnnotationType.Horizontal,
+                Y = avg,
+                Color = OxyColors.Red,
+                ClipByXAxis = false,
+                Text = "Avg"
+            });
 
             var absavg = ccr.Correlations.Average(f => Math.Abs(f));
-            var dsabsavg = new RawDataSource(new Point(0, absavg), new Point(xValues.Length - 1, absavg));
-            plotter.AddLineGraph(dsabsavg, Colors.Orange, 1d, "Abs Avg");
+            plotModel.Annotations.Add(new LineAnnotation {
+                Type = LineAnnotationType.Horizontal,
+                Y = absavg,
+                Color = OxyColors.Orange,
+                ClipByXAxis = false,
+                Text = "Abs Avg"
+            });
 
-            plotter.LegendVisible = false;
-            plotter.Viewport.Visible = new Rect(0, -1, xValues.Length, 2);
+            plotModel.IsLegendVisible = false;
+            plotter.Model = plotModel;
         }
     }
 }
