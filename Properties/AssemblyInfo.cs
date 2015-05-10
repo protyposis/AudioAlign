2203882dca2efe3bea4cece3324f7e829fe7bc84
Mario Guggenberger <mg@protyposis.net>
2015-05-10 18:56:21 +0200

    Move assembly description to assembly title
    The old assembly title is reflected in the build artifact's filename anyway, and the assembly description is shown in Windows explorer as assembly title, while the description is not shown anymore since Windows 7.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,8 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AudioAlign")]
-[assembly: AssemblyDescription("AudioAlign Audio Synchronization and Analysis Library")]
+[assembly: AssemblyTitle("AudioAlign Audio Synchronization and Analysis Library")]
 [assembly: AssemblyCopyright("Copyright © 2010-2015 Mario Guggenberger")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
